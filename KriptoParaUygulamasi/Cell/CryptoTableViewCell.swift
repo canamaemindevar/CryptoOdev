3a442d6b2b99fbb9189aa1e70f9d59507e037142
canamaemindevar <94639136+canamaemindevar@users.noreply.github.com>
2023-10-21 16:03:25 +0300

    small fix ✅

--- KriptoParaUygulamasi/Cell/CryptoTableViewCell.swift
+++ KriptoParaUygulamasi/Cell/CryptoTableViewCell.swift
@@ -8,11 +8,11 @@
 import UIKit
 
 final class CryptoTableViewCell: UITableViewCell {
-
+    
     static let identifier = "CryptoTableViewCell"
-
+    
     var upOrDownArr = [UIImage(systemName: "arrow.up")!,UIImage(systemName: "arrow.down")!]
-
+    
     var currency: Crypto? {
         didSet {
             self.currencyName.text = currency?.currency
@@ -34,54 +34,53 @@ final class CryptoTableViewCell: UITableViewCell {
         label.textColor = .lightGray
         return label
     }()
-   private let gainImageView: UIImageView = {
+    private let gainImageView: UIImageView = {
         let iv = UIImageView()
         iv.translatesAutoresizingMaskIntoConstraints = false
-        iv.image = .checkmark
         iv.contentMode = .scaleAspectFit
         iv.tintColor = .cyan
         return iv
     }()
-
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setup()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     override func prepareForReuse() {
         currency = nil
     }
-
+    
     private func setup() {
         contentView.addSubview(currencyName)
         contentView.addSubview(currencyprice)
         contentView.addSubview(gainImageView)
-
+        
         NSLayoutConstraint.activate([
             currencyName.leadingAnchor.constraint(equalTo: contentView.leadingAnchor,constant: 16),
             currencyName.topAnchor.constraint(equalTo: contentView.topAnchor),
             contentView.bottomAnchor.constraint(equalTo: currencyName.bottomAnchor),
             currencyName.widthAnchor.constraint(equalToConstant: self.frame.width / 2.5),
         ])
-
+        
         NSLayoutConstraint.activate([
             currencyprice.leadingAnchor.constraint(equalTo: currencyName.trailingAnchor),
             currencyprice.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
             currencyName.widthAnchor.constraint(equalToConstant: self.frame.width / 2.5),
             contentView.bottomAnchor.constraint(equalTo: currencyName.bottomAnchor),
         ])
-
+        
         NSLayoutConstraint.activate([
             gainImageView.leadingAnchor.constraint(equalTo: currencyprice.trailingAnchor,constant: 16),
             gainImageView.topAnchor.constraint(equalTo: contentView.topAnchor,constant: 16),
             bottomAnchor.constraint(equalTo: gainImageView.bottomAnchor,constant: 16),
             contentView.trailingAnchor.constraint(equalTo: gainImageView.trailingAnchor,constant: 8),
-           gainImageView.widthAnchor.constraint(equalToConstant: self.frame.width / 4),
+            gainImageView.widthAnchor.constraint(equalToConstant: self.frame.width / 4),
         ])
     }
-
+    
 }
--- KriptoParaUygulamasi/View/ViewController.swift
+++ KriptoParaUygulamasi/View/ViewController.swift
@@ -10,30 +10,28 @@ import RxSwift
 import RxCocoa
 
 final class ViewController: UIViewController {
-
+    
     var cryptoList = [Crypto]()
     let cryptoVM = CryptoViewModel()
     let disposeBag = DisposeBag()
-
+    
     private let tableView: UITableView = {
         let tableView = UITableView(frame: .zero,style: .grouped)
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.backgroundColor = .systemBackground
         tableView.separatorColor = .systemGray
         tableView.register(CryptoTableViewCell.self, forCellReuseIdentifier: CryptoTableViewCell.identifier)
-        tableView.layer.cornerRadius = 0
-
         return tableView
     }()
-
+    
     private let loader: UIActivityIndicatorView = {
         let loader = UIActivityIndicatorView()
         loader.translatesAutoresizingMaskIntoConstraints = false
-        loader.hidesWhenStopped = false
+        loader.hidesWhenStopped = true
         loader.color = .red
         return loader
     }()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
@@ -41,7 +39,7 @@ final class ViewController: UIViewController {
 }
 
 private extension ViewController {
-
+    
     func setup() {
         tableView.delegate = self
         tableView.dataSource = self
@@ -57,7 +55,7 @@ private extension ViewController {
             loader.widthAnchor.constraint(equalToConstant: view.frame.width / 5)
         ])
     }
-
+    
     func setupBindings() {
         cryptoVM
             .cryptos
@@ -66,14 +64,14 @@ private extension ViewController {
                 self.cryptoList = cryptos
                 self.tableView.reloadData()
             }.disposed(by: disposeBag)
-
+        
         cryptoVM
             .error
             .observe(on: MainScheduler.asyncInstance)
             .subscribe { error in
                 print(error)
             }.disposed(by: disposeBag)
-
+        
         cryptoVM
             .loading
             .bind(to: self.loader.rx.isAnimating)
@@ -82,19 +80,19 @@ private extension ViewController {
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return cryptoList.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: CryptoTableViewCell.identifier, for: indexPath) as? CryptoTableViewCell else {
             return UITableViewCell()}
-
+        
         cell.currency = cryptoList[indexPath.row]
         return cell
     }
-
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         self.view.frame.height / 10
     }
